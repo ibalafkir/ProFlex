1f2895ff450fe7948f5270bd5b74f6325aa57393
ismaelbd01 <ibalafkir@gmail.com>
2023-11-17 00:47:26 +0100

    Fixed rotation_translation function and added some interface analyser code

--- pydock_transformation.py
+++ pydock_transformation.py
@@ -1,31 +1,129 @@
 """
 ProFlex PyDock poses filtration
 """
+# TODO Que funcione interface analyser dentro de rotate_nd_translate, ver si quitar returns
+######################## INTERFACE ANALYSER CODE EXTRACTED FROM interface_analyser.py eacbc3c ###########
 import argparse
 from biopandas.pdb import PandasPdb
 import numpy as np
 import pandas as pd
 def get_pdb_atoms_df(pdb_file):
     ppdb = PandasPdb().read_pdb(pdb_file)
-    print("Reading PDB file")
-    print(" ")
     return ppdb.df['ATOM'] # only keeps information related to atoms
 def get_ca(atom_df):
-    print("Filtering CA")
-    print(" ")
     return atom_df[atom_df['atom_name'] == 'CA']
 def get_relevant_columns(chain):
     desired_cols = ['chain_id', 'residue_number', 'residue_name', 'x_coord', 'y_coord', 'z_coord']
     x = chain[desired_cols]
-    print("Filtering relevant columns of chain", chain['chain_id'].values[0])
-    print(" ")
     return x
+
+def get_interface_residues_by_chain(chain1: str, chain2: str, distance_threshold=6) -> (np.array, np.array, np.array):
+    coords_1 = chain1[['x_coord', 'y_coord', 'z_coord']].values
+    coords_2 = chain2[['x_coord', 'y_coord', 'z_coord']].values
+    distances = np.linalg.norm(coords_1[:, None] - coords_2, axis=2)
+    close_residues = np.where(distances <= distance_threshold)
+    residues_interface_1 = chain1.iloc[close_residues[0]]
+    residues_interface_2 = chain2.iloc[close_residues[1]]
+    detected_distances = pd.DataFrame({
+        'First chain residue': residues_interface_1['residue_name'].values,
+        'Residue number 1': residues_interface_1['residue_number'].values,
+        'X_1': residues_interface_1['x_coord'].values,     # REMOVE?
+        'Y_1': residues_interface_1['y_coord'].values,     # REMOVE?
+        'Z_1': residues_interface_1['z_coord'].values,     # REMOVE?
+        'Second chain residue': residues_interface_2['residue_name'].values,
+        'Residue number 2': residues_interface_2['residue_number'].values,
+        'X_2': residues_interface_2['x_coord'].values,     # REMOVE?
+        'Y_2': residues_interface_2['y_coord'].values,     # REMOVE?
+        'Z_2': residues_interface_2['z_coord'].values,     # REMOVE?
+        'Distance': distances[close_residues]
+    })
+    return residues_interface_1, residues_interface_2, detected_distances
+
+def amplify_selection_residues(int, chainrelevant):
+    i = 0
+    total_dif = []
+    res_num = sorted(int['residue_number'].values)
+    while i < len(res_num)-1:
+        subs = res_num[i+1]-res_num[i]
+        total_dif.append(subs)
+        if subs == 2:
+            res_num_to_select = res_num[i]+1
+            residue_to_add = chainrelevant.loc[chainrelevant['residue_number'] == res_num_to_select]
+            int = int._append(residue_to_add)
+
+        if subs == 3:
+            res_num_to_select = [res_num[i+1]-1, res_num[i]+1]
+            residue_to_add1 = chainrelevant.loc[chainrelevant['residue_number'] == res_num_to_select[0]]
+            residue_to_add2 = chainrelevant.loc[chainrelevant['residue_number'] == res_num_to_select[1]]
+            int = int._append(residue_to_add1)
+            int = int._append(residue_to_add2)
+
+        if subs == 4:
+            res_num_to_select = [res_num[i + 1] - 1, res_num[i] + 1, res_num[i] + 2]
+            residue_to_add1 = chainrelevant.loc[chainrelevant['residue_number'] == res_num_to_select[0]]
+            residue_to_add2 = chainrelevant.loc[chainrelevant['residue_number'] == res_num_to_select[1]]
+            residue_to_add3 = chainrelevant.loc[chainrelevant['residue_number'] == res_num_to_select[2]]
+            int = int._append(residue_to_add1)
+            int = int._append(residue_to_add2)
+            int = int._append(residue_to_add3)
+        i += 1
+    int_sorted = int.sort_values(by='residue_number')
+    int_sorted_unique = int_sorted.drop_duplicates()
+    return int_sorted_unique
+
+def get_interface_residues_by_each_chain(pdb_file, distance_threshold=6) -> (np.array, np.array, np.array, np.array):
+    atom_df = get_pdb_atoms_df(pdb_file)
+    atom_df_ca = get_ca(atom_df)
+    chain_ids = get_chains_id(atom_df_ca)
+    chain_1 = get_chain(atom_df_ca, chain_ids[0])
+    chain_2 = get_chain(atom_df_ca, chain_ids[1])
+    chain1_relevant = get_relevant_columns(chain_1)
+    chain2_relevant = get_relevant_columns(chain_2)
+    int1, int2, interactions = get_interface_residues_by_chain(chain1_relevant, chain2_relevant, distance_threshold)
+    intchain1additional = amplify_selection_residues(int1, chain1_relevant)
+    intchain2additional = amplify_selection_residues(int2, chain2_relevant)
+    return int1, int2, interactions, intchain1additional, intchain2additional
+
+#################################
 def get_only_coords(chain):
     desired_cols = ['x_coord', 'y_coord', 'z_coord']
     x = chain[desired_cols]
     print("Getting only coordinates")
     print(" ")
     return x
+
+def rotate_and_translate(coords, rotation_translation):
+    """Applies rotation and translation matrix to a set of coordinates."""
+    new_coords = []
+    i = 0
+    while i <= len(rotation_translation)-1:
+        for coord in coords.values:
+            new_x = (rotation_translation.values[i][0] * coord[0] +
+                     rotation_translation.values[i][1] * coord[1] +
+                     rotation_translation.values[i][2] * coord[2] +
+                     rotation_translation.values[i][9])
+            new_y = (rotation_translation.values[i][3] * coord[0] +
+                     rotation_translation.values[i][4] * coord[1] +
+                     rotation_translation.values[i][5] * coord[2] +
+                     rotation_translation.values[i][10])
+            new_z = (rotation_translation.values[i][6] * coord[0] +
+                     rotation_translation.values[i][7] * coord[1] +
+                     rotation_translation.values[i][8] * coord[2] +
+                     rotation_translation.values[i][11])
+            new_coords.append([new_x, new_y, new_z])
+
+            if len(new_coords) == len(coords):
+                df = pd.DataFrame(new_coords, columns=['x_coord', 'y_coord', 'z_coord'])
+                i += 1
+                print("Log: advancing till the rot+transl matrix row number:", i)
+
+                # test interface
+
+                new_coords = []
+
+    return df
+
+hola = rotate_and_translate(lcotest,rttest)
 
 pdb_file_rec = "apo_pd1_6umv_noMt_optH_minH-DOCK-Q15116_20_1.51_igfold_imgt_rec.pdb"
 pdb_file_lig = "apo_pd1_6umv_noMt_optH_minH-DOCK-Q15116_20_1.51_igfold_imgt_lig.pdb"
@@ -46,27 +144,4 @@ rotation_translation = pd.read_table(rot_file_pydock, delim_whitespace=True, hea
     'Label'
 ])
 
-############ good till here
-def rotate_and_translate(coords, rotation_translation): # FALLA ESTA FUNCION
-                                                        # Me quede por ver como sacar columna entera
-    """Applies rotation and translation matrix to a set of coordinates."""
-    new_coords = []
-    i = 0
-    for coord in coords.values:
-        new_x = (rotation_translation.values[i][0] * coord[0] +
-                     rotation_translation.values[i][1] * coord[1] +
-                     rotation_translation.values[i][2] * coord[2] +
-                     rotation_translation.values[i][9])
-        new_y = (rotation_translation.values[i][3] * coord[0] +
-                     rotation_translation.values[i][4] * coord[1] +
-                     rotation_translation.values[i][5] * coord[2] +
-                     rotation_translation.values[i][10])
-        new_z = (rotation_translation.values[i][6] * coord[0] +
-                     rotation_translation.values[i][7] * coord[1] +
-                     rotation_translation.values[i][8] * coord[2] +
-                     rotation_translation.values[i][11])
-        new_coords.append((new_x, new_y, new_z))
-        i += 1
-        df = pd.DataFrame(new_coords, columns=['x_coord', 'y_coord', 'z_coord'])
-    return df
-lig_coord_after_rot_trans = rotate_and_translate(lig_coord_only, rotation_translation)
+
