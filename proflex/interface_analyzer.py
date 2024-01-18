cf71e1602bfeac937eee20cc692876f0b234a54c
annadiarov <annadiarov@gmail.com>
2024-01-18 13:12:46 +0100

    fix: Adapt interface_analyzer to use proflex.utils.PDBUtils

--- proflex/interface_analyzer.py
+++ proflex/interface_analyzer.py
@@ -5,68 +5,9 @@ both chain IDs to study interactions and a maximum distance threshold up to whic
 to be interacting. The user needs to select which chains will study.
 """
 import argparse
-from biopandas.pdb import PandasPdb
 import numpy as np
 import pandas as pd
-
-class PDBUtils:
-    def __init__(self, pdb_file):
-        self.pdb_file = pdb_file
-
-    def get_pdb_atoms_df(self):
-        """
-        Extracts ATOM information from a PDB file and returns a pandas DataFrame
-        :param Path to PDB file: str
-        :return Pandas DataFrame: pandas.DataFrame
-        """
-        ppdb = PandasPdb().read_pdb(pdb_file)
-        print("Extracting atom information from PDB... \n")
-        return ppdb.df['ATOM'] # only keeps information related to atoms
-    @staticmethod
-    def get_ca(atom_df):
-        """
-        Extracts CA rows from a pandas DataFrame with ATOM information
-        :param Pandas DataFrame with ATOM information: pandas.DataFrame
-        :return Pandas DataFrame with CA information: pandas.DataFrame
-        """
-        print("Getting alpha carbons... \n")
-        return atom_df[atom_df['atom_name'] == 'CA']
-    @staticmethod
-    def delete_res_tag(df):
-        def delete_numbers_in_string(string):
-            if isinstance(string, str):
-                return ''.join(c for c in string if c.isdigit())
-            else:
-                return str(string)
-
-        # Uses .loc to avoid SettingWithCopyWarning
-        df.loc[:, 'residue_number'] = df['residue_number'].apply(delete_numbers_in_string)
-        df['residue_number'] = df['residue_number'].astype(int)
-        return df
-
-    @staticmethod
-    def get_chain(df, chain_name):
-        """
-        Returns pandas DataFrame with the ATOM information of an input chain
-        :param Pandas DataFrame with ATOM information (CA atom_names, all atom_names...):
-               pandas.DataFrame
-        :return Pandas DataFrame with ATOM information of an indicated chain:
-                pandas.DataFrame
-        """
-        print("Obtaining different protein chains...:", chain_name, "\n")
-        return df[df['chain_id'] == chain_name]
-    @staticmethod
-    def get_relevant_columns(chain):
-        """
-        Extracts "relevant columns" from an ATOM PDB pandas DatFrame. These are:
-        chain_id, residue_number, residue_name, x_coord, y_coord, z_coord
-        :param Atom DataFrame: pandas.DataFrame
-        :return Atom DataFrame with relevant columns
-        """
-        desired_cols = ['chain_id', 'residue_number', 'residue_name', 'x_coord', 'y_coord', 'z_coord']
-        x = chain[desired_cols]
-        print("Filtering relevant columns of chain", chain['chain_id'].values[0], "... \n")
-        return x
+from proflex.utils import PDBUtils
 
 def get_interface_residues_by_chain(chain1: str, chain2: str, distance_threshold=6):
     """
@@ -173,14 +114,13 @@ def amplify_selection_residues(int, chainrelevant):
 
 def analyze_interface(pdb_file, id1, id2, distance_threshold):
 
-    pdb_utils_instance = PDBUtils(pdb_file)
-    atom_df = pdb_utils_instance.get_pdb_atoms_df()
-    atom_df = pdb_utils_instance.delete_res_tag(atom_df)
-    atom_df_ca = pdb_utils_instance.get_ca(atom_df)
-    chain_1 = pdb_utils_instance.get_chain(atom_df_ca, id1)
-    chain_2 = pdb_utils_instance.get_chain(atom_df_ca, id2)
-    chain1_relevant = pdb_utils_instance.get_relevant_columns(chain_1)
-    chain2_relevant = pdb_utils_instance.get_relevant_columns(chain_2)
+    atom_df = PDBUtils.get_pdb_atoms_df(pdb_file)
+    atom_df = PDBUtils.delete_res_tag(atom_df)
+    atom_df_ca = PDBUtils.get_ca(atom_df)
+    chain_1 = PDBUtils.get_chain(atom_df_ca, id1)
+    chain_2 = PDBUtils.get_chain(atom_df_ca, id2)
+    chain1_relevant = PDBUtils.get_relevant_columns(chain_1)
+    chain2_relevant = PDBUtils.get_relevant_columns(chain_2)
     int1, int2 = get_interface_residues_by_chain(chain1_relevant, chain2_relevant, distance_threshold)
     intchain1additional = amplify_selection_residues(int1, chain1_relevant)
     intchain2additional = amplify_selection_residues(int2, chain2_relevant)
