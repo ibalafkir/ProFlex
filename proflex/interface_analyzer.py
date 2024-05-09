4e84f014723a644263771b731579fbbdfd2d7e94
ismaelbd01 <ibalafkir@gmail.com>
2024-05-09 08:38:48 +0200

    Minor change in interface_analyzer

--- proflex/interface_analyzer.py
+++ proflex/interface_analyzer.py
@@ -8,6 +8,7 @@ import argparse
 import pandas as pd
 from proflex.pdb import PdbDf
 from proflex.interface import InterfaceAnalyzer
+import os
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Calculates interface residues between two chains of PDB file')
@@ -21,22 +22,22 @@ if __name__ == "__main__":
     ab1 = args.ab1
     ab2 = args.ab2
     ag = args.ag
-    
+
     distance_threshold = args.distance_threshold
     pd.set_option('display.max_rows', None)
     pd.set_option('display.max_columns', None)
-    
+
 
     # For NANOBODIES
-    
+
     if ab1 and not ab2:
-    
+
         int1, int2, detected_interactions, int1_extended, int2_extended = InterfaceAnalyzer.run(pdb, args.ab1, args.ag, distance_threshold)
-        print("----------------------------------------------------------------------\n")
-        print("[INFO] Running ProFlex InterfaceAnalyzer for a nanobody-antigen system\n")
-        print("----------------------------------------------------------------------\n")
-        print(f"[INFO] 1) CHAINS {ab1} AND {ag}\n")    
-        
+        print("--------------------------------------------------------------------------------------------------------------\n")
+        print(f"[INFO] Running ProFlex InterfaceAnalyzer for a nanobody-antigen system: {os.path.basename(pdb)}\n")
+        print("--------------------------------------------------------------------------------------------------------------\n")
+        print(f"[INFO] 1) CHAINS {ab1} AND {ag}\n")
+
         #print("[INFO]Interface residues from chain", ab1)
         #print(int1, "\n")
         #print("[INFO] Interface residues from chain", ag)
@@ -54,10 +55,10 @@ if __name__ == "__main__":
 
     if ab1 and ab2:
         print("----------------------------------------------------------------------\n")
-        print("[INFO] ProFlex InterfaceAnalyzer for an antibody-antigen system\n")
-        print("----------------------------------------------------------------------\n") 
+        print(f"[INFO] ProFlex InterfaceAnalyzer for an antibody-antigen system: {os.path.basename(pdb)}\n")
+        print("----------------------------------------------------------------------\n")
 
-        int1_1, int2_1, detected_interactions, int1_1_extended, int2_1_extended = InterfaceAnalyzer.run(pdb, args.ab1, args.ag, distance_threshold)      
+        int1_1, int2_1, detected_interactions, int1_1_extended, int2_1_extended = InterfaceAnalyzer.run(pdb, args.ab1, args.ag, distance_threshold)
         print(f"[INFO] 1) CHAINS {ab1} AND {ag}")
         #print("[INFO]Interface residues from chain", ab1)
         #print(int1_1, "\n")
@@ -72,7 +73,7 @@ if __name__ == "__main__":
         print("[INFO] Code for PELE com_distance metric")
         InterfaceAnalyzer.pele_com_distance(int1_1, int2_1, ab1, ag)
 
-        print(f"\n[INFO] 2) CHAINS {ab2} AND {ag}")  
+        print(f"\n[INFO] 2) CHAINS {ab2} AND {ag}")
         int1_2, int2_2, detected_interactions, int1_2_extended, int2_2_extended = InterfaceAnalyzer.run(pdb, args.ab2, args.ag, distance_threshold)
         #print("\n[INFO] Interface residues from chain", ab2)
         #print(int1_2, "\n")
@@ -86,13 +87,13 @@ if __name__ == "__main__":
         #print(intchain2_extended, "\n")
         print("[INFO] Code for PELE com_distance metric")
         InterfaceAnalyzer.pele_com_distance(int1_2, int2_2, ab2, ag)
-        
-        
-        
-        
+
+
+
+
                             ####### Residues to omit ######## TODO Create a PELETask builder in a class and include this
-        
-        print(f"\n[INFO] SELECTIONS TO OMIT (residues with CA distance < 8 Angstrom)")
+
+        print(f"\n[INFO] SELECTIONS TO OMIT (residues with CA distance < 10 Angstrom)")
         def del_repeated(lst):
             """
             Deletes repeated elements in a list
@@ -104,25 +105,25 @@ if __name__ == "__main__":
                 else:
                     continue
             return result
-        
+
         # Analyze antibodies interface
-        ab1, ab2, d, int1_ex, int2_ex = InterfaceAnalyzer.run(pdb, args.ab1, args.ab2, distance_threshold=8)
-        
-        # Selected residues with the antigen
-        ab1Antigen_lst = int1_1['residue_number'].to_list()
-        ab2Antigen_lst = int1_2['residue_number'].to_list()     
+        ab1, ab2, d, ab1_extended, ab2_extended = InterfaceAnalyzer.run(pdb, args.ab1, args.ab2, distance_threshold=10)
+
+        # Selected residues with the antigen amplified by neighborhood (we dont want to omit anything who has interface selected res around)
+        ab1Antigen_lst = int1_1_extended['residue_number'].to_list()
+        ab2Antigen_lst = int1_2_extended['residue_number'].to_list()
         ab1Antigen_lst = sorted(del_repeated(ab1Antigen_lst))
         ab2Antigen_lst = sorted(del_repeated(ab2Antigen_lst))
-        
+
         # Making the selection to omit tag
-        
-        ab1_lst = ab1['residue_number'].to_list()
-        ab2_lst = ab2['residue_number'].to_list()
+
+        ab1_lst = ab1_extended['residue_number'].to_list() # we get the extended list because we want to omit the neighbors between omitted residues
+        ab2_lst = ab2_extended['residue_number'].to_list()
         ab1_lst = sorted(del_repeated(ab1_lst))
         ab2_lst = sorted(del_repeated(ab2_lst))
         #print("Esto es ab1_lst", ab1_lst)
         #print("Esto es ab2_lst", ab2_lst)
-        
+
         ab1_lst_updated = []
         for element in ab1_lst:
             if element in ab1Antigen_lst:
@@ -135,10 +136,10 @@ if __name__ == "__main__":
                 continue
             else:
                 ab2_lst_updated.append(element)
-        
+
         ab1_lst_updated = sorted(del_repeated(ab1_lst_updated))
         ab2_lst_updated = sorted(del_repeated(ab2_lst_updated))
-        
+
         ab1_lst_updated_chname = [f"{args.ab1}:" + str(element) for element in ab1_lst_updated]
         ab1_lst_updated_chname_mod = ['"' + element + '"' for element in ab1_lst_updated_chname]
         pele_code_1 = ', '.join(ab1_lst_updated_chname_mod)
@@ -147,5 +148,5 @@ if __name__ == "__main__":
         ab2_lst_updated_chname = [f"{args.ab2}:" + str(element) for element in ab2_lst_updated]
         ab2_lst_updated_chname_mod = ['"' + element + '"' for element in ab2_lst_updated_chname]
         pele_code_2 = ', '.join(ab2_lst_updated_chname_mod)
-        
+
         print('"selectionToOmit": { "links": { "ids":[' + str(pele_code_1) + ', ' + str(pele_code_2) + ']}},')
